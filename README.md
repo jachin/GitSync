42bfc8d5df9e362ccbda577735d9884c9d72c054
Jachin Rupe <jachin.rupe@gmail.com>
2011-11-30 01:22:58 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+GitSync
+=======
+
+OS X Dependencies
+-----------------
+
+Skip any steps that install software you already have.
+
+1. Install Homebrew 
+   http://mxcl.github.com/homebrew/
+2. Install git 
+   ```brew install git```
+3. Install python
+   ```brew install python```
+4. Install pip
+   ```sudo easy_install pip```
+5. Install fabric
+   ```sudo pip install fabric```
+6. Install the python library of Mac FS Events
+   ```sudo pip install MacFSEvents```
+7. Install the python binding for growl.
+   ```sudo pip install gntp```
+
+
+Configuration
+-------------
+
---
+++ git_sync.py
@@ -0,0 +1,109 @@
+import os
+import subprocess
+import ConfigParser
+import argparse
+import pprint
+import logging
+
+from fsevents import Observer
+from fsevents import Stream
+
+import gntp
+import gntp.notifier
+
+parser = argparse.ArgumentParser(
+    description='Use git to sync a site on pion to your local machine.'
+)
+
+parser.add_argument(
+    'config_file'
+    , nargs='?'
+    , type=argparse.FileType('r')
+    , default="./git_sync.cfg"
+)
+
+args = parser.parse_args()
+
+config = ConfigParser.ConfigParser()
+
+config.readfp(args.config_file)
+
+local_path    =  config.get('GitSync', 'local_path')
+local_branch  =  config.get('GitSync', 'local_branch')
+remote_path   =  config.get('GitSync', 'remote_path')
+remote_host   =  config.get('GitSync', 'remote_host')
+remote_user   =  config.get('GitSync', 'remote_user')
+
+if remote_user:
+	remote_host  =  remote_user + '@' + remote_host
+
+growl = gntp.notifier.GrowlNotifier(
+<<<<<<< HEAD
+    applicationName = "GitSync",
+    notifications = ["Sync Starting", "Sync Done"],
+    defaultNotifications = ["Sync Done"],
+    port = 23053,
+    hostname = "localhost",
+=======
+	applicationName = "GitSync",
+	notifications = ["Sync Starting", "Sync Done"],
+	defaultNotifications = ["Sync Starting","Sync Done"],
+	hostname = "localhost",
+>>>>>>> das
+)
+
+pprint.pprint( growl.register() )
+
+def growl_start( ):
+	growl.notify(
+		noteType = "Sync Starting",
+		title = "Sync is starting",
+		description = "Starting to sync local_path and remote_path on remote_host",
+	)
+
+def growl_done( ):
+	growl.notify(
+		noteType = "Sync Done",
+		title = "Sync is finished",
+		description = "Completed sync of local_path and remote_path on remote_host",
+	)
+
+<<<<<<< HEAD
+#growl.notify(
+#   noteType = "Sync Starting",
+#   title = "Sync is starting",
+#   description = "Starting to sync local_path and amm_name",
+#)
+=======
+>>>>>>> das
+
+def callback( event ):
+
+    filename = event.name
+    git_dir = os.path.join( local_path, '.git' )
+
+<<<<<<< HEAD
+    #Skip sync for file change that are in the .git directory.
+    if not git_dir in filename:
+        subprocess.call("fab sync:amm_name=%s,local_path=%s" % ( amm_name, local_path ), shell=True)
+        print 'To stop: Ctrl-\\'
+=======
+	#Skip sync for file change that are in the .git directory.
+	if not git_dir in filename:
+		growl_start( )
+		subprocess.call("fab -H %s sync:remote_path=%s,local_path=%s,local_branch=%s" % ( remote_host, remote_path, local_path, local_branch ), shell=True)
+		growl_done( )
+		print 'To stop: Ctrl-\\'
+>>>>>>> das
+
+# Do an initial sync
+growl_start( )
+subprocess.call("fab -H %s sync:remote_path=%s,local_path=%s,local_branch=%s" % ( remote_host, remote_path, local_path, local_branch ), shell=True)
+growl_done( )
+print 'To stop: Ctrl-\\'
+
+# Start watching the directory
+observer = Observer()
+stream = Stream(callback, local_path, file_events=True)
+observer.schedule(stream)
+observer.start()
