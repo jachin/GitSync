6042eaebcc7bafcc69c342e5d6c318b61ad36a5c
Jachin Rupe <jachin.rupe@gmail.com>
2011-12-19 17:27:22 -0600

    The init processes now sets up the local branch correctly.

--- git_sync.py
+++ git_sync.py
@@ -19,7 +19,7 @@ import gntp
 import gntp.notifier
 
 @task
-def init_remote_master_repository( remote_path, local_branch ):
+def init_remote_master_repository( remote_path, local_branch, git_ignore_lines ):
 
     puts("Setting up %s" % remote_path)
 
@@ -33,10 +33,10 @@ def init_remote_master_repository( remote_path, local_branch ):
     else:
         with cd ( remote_path ):
             run( "git init" )
-            append( '.gitignore', '.DS_Store' )
-            append( '.gitignore', '.svn*' )
-            append( '.gitignore', 'data/asset/*' )
-            append( '.gitignore', 'data/tmp/*' )
+
+        update_git_ignore_file( remote_path, git_ignore_lines )
+
+        with cd ( remote_path ):
             run( "git add .gitignore" )
             run( "git commit -m 'Inital Commit'" )
             run( "git add ." )
@@ -61,13 +61,21 @@ def remote_has_modified_files( remote_path ):
     with cd ( remote_path ):
         with settings( warn_only=True ):
             with hide('running', 'status', 'warnings', 'stderr', 'stdout'):
+
                 git_status_output = run( "git status . " )
-                puts(red(git_status_output))
-                if "nothing to commit (working directory clean)" in git_status_output:
+
+                git_clean_status = "nothing to commit (working directory clean)"
+                puts( red( git_status_output ) )
+                if git_clean_status in git_status_output:
                     puts(cyan("%s (remote) is clean." % remote_path))
                     return False
                 else:
-                    puts(cyan("%s (remote) has uncommitted changes." % remote_path))
+                    puts(
+                        cyan(
+                            " %s (remote) has uncommitted changes."
+                            % remote_path
+                        )
+                    )
                     return True
 
 
@@ -76,12 +84,17 @@ def local_has_modified_files ( local_path ):
     with lcd ( local_path ):
         with settings( warn_only=True ):
             with hide('running', 'status', 'warnings', 'stderr', 'stdout'):
+
                 git_status_output = local( "git status . ", capture=True )
-                if "nothing to commit (working directory clean)" in git_status_output:
-                    puts(cyan("%s (local) is clean." % local_path))
+
+                git_clean_status = "nothing to commit (working directory clean)"
+                if git_clean_status in git_status_output:
+                    puts( cyan("%s (local) is clean." % local_path ) )
                     return False
                 else:
-                    puts(cyan("%s (local) has uncommitted changes." % local_path))
+                    puts(
+                        cyan( "%s (local) has uncommitted changes." % local_path )
+                    )
                     return True
 
 
@@ -100,7 +113,7 @@ def get_local_git_clone ( remote_path, local_path ):
 def commit_remote_modified_files ( remote_path ):
     if not remote_has_modified_files( remote_path ):
         return True
-    with cd ( remote_path ):
+    with cd( remote_path ):
         run( "git add ." )
         run( "git commit -a -m 'committing all changes from %s'" % (remote_path) )
         return True
@@ -108,15 +121,24 @@ def commit_remote_modified_files ( remote_path ):
 
 @task
 def push_remote_master ( remote_path, local_branch ):
+
+    remote_has_local_branch( remote_path, local_branch )
+
     with cd ( remote_path ):
         run( "git push origin %s" % (local_branch) )
         return True
 
 
+def remote_has_local_branch ( remote_path, local_branch ):
+    with cd ( remote_path ):
+        git_branches = run ( 'git branch' )
+        puts( cyan( git_branches ) )
+
+
 @task
 def commit_and_push_remote ( remote_path ) :
-    commit_amm_modified_files(amm_name)
-    push_amm_master(amm_name)
+    commit_amm_modified_files( amm_name )
+    push_amm_master( amm_name )
 
 
 @task
@@ -150,8 +172,28 @@ def commit_local_modified_files ( local_path ):
 
 @task
 def push_local_to_remote ( local_path, local_branch ):
+    if not local_has_local_branch( local_path, local_branch ):
+        local_create_local_branch( local_path, local_branch )
+
     with lcd ( local_path ):
-        local( "git push origin %s" % (local_branch) )
+        local( "git push origin %s" % ( local_branch ) )
+
+
+def local_create_local_branch ( local_path, local_branch ):
+    with lcd ( local_path ):
+        local ( 'git branch %s' % ( local_branch ), capture=True )
+
+
+def local_has_local_branch ( local_path, local_branch ):
+
+    puts( cyan( local_path ) )
+
+    with lcd ( local_path ):
+        git_branches = local ( 'git branch', capture=True )
+        for branch in git_branches.split( ):
+            if branch == local_branch:
+                return True
+        return False
 
 
 @task
@@ -161,24 +203,24 @@ def merge_local_to_remote ( remote_path, local_branch ):
 
 
 @task
-def send_local_changes_to_remote( remote_path, local_path, local_branch ):
+def send_local_changes_to_remote ( remote_path, local_path, local_branch ):
     commit_local_modified_files( local_path )
     push_local_to_remote( local_path, local_branch )
     merge_local_to_remote( remote_path, local_branch )
 
 
 @task
-def send_remote_changes_to_local( remote_path, local_path ):
+def send_remote_changes_to_local ( remote_path, local_path ):
     commit_remote_modified_files( remote_path )
     pull_and_merge_local( local_path )
 
 
 @task
-def sync( remote_path, local_path, local_branch ):
+def sync ( remote_path, local_path, local_branch, git_ignore_lines ):
     puts( blue( 'sync' ) )
 
     if not os.path.exists( local_path ):
-        init( remote_path, local_path, local_branch )
+        init( remote_path, local_path, local_branch, git_ignore_lines )
 
     if remote_has_modified_files( remote_path ):
         send_remote_changes_to_local( remote_path, local_path )
@@ -187,9 +229,12 @@ def sync( remote_path, local_path, local_branch ):
 
 
 @task
-def init ( remote_path, local_path, local_branch ):
-    init_remote_master_repository( remote_path, local_branch )
+def init ( remote_path, local_path, local_branch, git_ignore_lines ):
+    init_remote_master_repository( remote_path, local_branch, git_ignore_lines )
     get_local_git_clone ( remote_path, local_path )
+    local_create_local_branch( local_path, local_branch )
+    with lcd( local_path ):
+        local( "git checkout %s" % (local_branch) )
 
 
 # Setup Parser
@@ -269,10 +314,11 @@ def callback( event ):
         growl_start( )
         execute(
             sync
-            , host         = remote_host
-            , remote_path  = remote_path
-            , local_path   = local_path
-            , local_branch = local_branch
+            , host             = remote_host
+            , remote_path      = remote_path
+            , local_path       = local_path
+            , local_branch     = local_branch
+            , git_ignore_lines = git_ignore_lines
         )
         growl_done( )
         print 'To stop: Ctrl-\\'
@@ -282,22 +328,16 @@ growl_start( )
 
 execute(
     sync
-    , host         = remote_host
-    , remote_path  = remote_path
-    , local_path   = local_path
-    , local_branch = local_branch
+    , host             = remote_host
+    , remote_path      = remote_path
+    , local_path       = local_path
+    , local_branch     = local_branch
+    , git_ignore_lines = git_ignore_lines
 )
 
 growl_done( )
 
 print 'To stop: Ctrl-\\'
-
-execute(
-     update_git_ignore_file
-    , host = remote_host
-    , remote_path = remote_path
-    , git_ignore_lines=git_ignore_lines
-)
 
 # Start watching the directory
 observer = Observer()
