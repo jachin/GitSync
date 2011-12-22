683d8176e7060b8be0144f09a2dc5e7a56cf72ba
Jachin Rupe <jachin.rupe@gmail.com>
2011-12-21 18:06:52 -0600

    Adding some stuff for testing.

--- git_sync.py
+++ git_sync.py
@@ -50,7 +50,6 @@ def update_git_ignore_file( remote_path, git_ignore_lines ):
             if not contains( '.gitignore_new', line ):
                 append( '.gitignore_new', line )
 
-        run('rm .gitignore', shell=False)
         run('mv .gitignore_new .gitignore', shell=False)
 
 
@@ -308,6 +307,7 @@ def callback( event ):
 
     filename = event.name
     git_dir  = os.path.join( local_path, '.git' )
+    pprint.pprint(event)
 
     #Skip sync for file change that are in the .git directory.
     if not git_dir in filename:
@@ -344,3 +344,29 @@ observer = Observer()
 stream = Stream(callback, local_path, file_events=True)
 observer.schedule(stream)
 observer.start()
+
+
+
+# enum { 
+#     kFSEventStreamEventFlagNone = 0x00000000, 
+#     kFSEventStreamEventFlagMustScanSubDirs = 0x00000001, 
+#     kFSEventStreamEventFlagUserDropped = 0x00000002, 
+#     kFSEventStreamEventFlagKernelDropped = 0x00000004, 
+#     kFSEventStreamEventFlagEventIdsWrapped = 0x00000008, 
+#     kFSEventStreamEventFlagHistoryDone = 0x00000010, 
+#     kFSEventStreamEventFlagRootChanged = 0x00000020, 
+#     kFSEventStreamEventFlagMount = 0x00000040, 
+#     kFSEventStreamEventFlagUnmount = 0x00000080, /* These flags are only set if you specified the FileEvents*/
+#     /* flags when creating the stream.*/
+#     kFSEventStreamEventFlagItemCreated = 0x00000100, 
+#     kFSEventStreamEventFlagItemRemoved = 0x00000200, 
+#     kFSEventStreamEventFlagItemInodeMetaMod = 0x00000400, 
+#     kFSEventStreamEventFlagItemRenamed = 0x00000800, 
+#     kFSEventStreamEventFlagItemModified = 0x00001000, 
+#     kFSEventStreamEventFlagItemFinderInfoMod = 0x00002000, 
+#     kFSEventStreamEventFlagItemChangeOwner = 0x00004000, 
+#     kFSEventStreamEventFlagItemXattrMod = 0x00008000, 
+#     kFSEventStreamEventFlagItemIsFile = 0x00010000, 
+#     kFSEventStreamEventFlagItemIsDir = 0x00020000, 
+#     kFSEventStreamEventFlagItemIsSymlink = 0x00040000 
+# };
