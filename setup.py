1d5070755c4c5ce670cbd4a1c95c043d7bd92b0e
Jachin Rupe <jachin.rupe@gmail.com>
2014-03-06 15:04:36 -0600

    Upping the version number.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='GitSync',
-    version='0.1.0',
+    version='0.1.1',
     author='Jachin Rupe',
     author_email='jachin@clockwork.net',
     packages=['gitsync'],
