5150527cdb6d14bc34b99a681d1938c8434671f7
Jachin Rupe <jachin.rupe@gmail.com>
2014-03-06 14:41:30 -0600

    Adding pync to the dependences list and some classifiers for the cheese shop.

--- setup.py
+++ setup.py
@@ -12,9 +12,19 @@ setup(
     description='Use git to sync a project directory on an OS X client with a'
                 ' remote server.',
     long_description=open('README.txt').read(),
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Environment :: MacOS X',
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Topic :: Communications :: File Sharing',
+        'Topic :: Utilities',
+    ],
     install_requires=[
         "Fabric >= 1.8.0",
         "MacFSEvents >= 0.3",
         "PyYAML >= 3.10",
+        "pync >= 1.4",
     ],
 )
